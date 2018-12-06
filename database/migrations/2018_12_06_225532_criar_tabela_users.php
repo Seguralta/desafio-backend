cef59118f73078de664c8c296d8d6afcffdf39e9
Tiago Kinust Biage <tiago.biage@title.com.br>
2018-12-06 21:12:21 -0200

    create migrations users

--- database/migrations/2018_12_06_225532_criar_tabela_users.php
+++ database/migrations/2018_12_06_225532_criar_tabela_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CriarTabelaUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        $this->down();
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 255);
+            $table->string('last_name', 255);
+            $table->string('email', 255);
+            $table->string('password', 255);
+            $table->string('cpf', 15);
             $table->timestamps();
         });
     }
