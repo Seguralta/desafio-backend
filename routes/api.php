2e402246d40a007904faa63207882be36095e885
Tiago Kinust Biage <tiago.biage@title.com.br>
2018-12-06 15:51:45 -0200

    autenticação do header

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('authapi')->post('/user', function (Request $request) {
+    echo 'ok';
+    //return $request->user();
+});
